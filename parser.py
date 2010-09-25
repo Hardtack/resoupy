4e3c065169ac1a365a15869937e95c20a83869fd
Hardtack <6566gun@gmail.com>
2010-09-25 18:11:08 +0900

    Modified parser and symbol

--- parser.py
+++ parser.py
@@ -1,13 +1,15 @@
 from symbol import *
 from functions import *
 def parse(tokenQueue):
+    listStack = []
     while len(tokenQueue):
         token = tokenQueue.pop(0)
-        listStack = []
         if token is '(':
-            listStack.push(list)
+            print '(', len(listStack)
+            listStack.append([])
 
         elif token is ')':
+            print ')', len(listStack)
             list = listStack.pop()
             if len(listStack) is 0:
                 return list
@@ -15,13 +17,22 @@ def parse(tokenQueue):
             listStack[-1].append(list)
 
         elif token[0] is '"':
+            print token, len(listStack)
             string = token[1:-1]
             if len(listStack) is 0:
                 return string
 
             listStack[-1].append(string)
 
+        elif isNumber(token):
+            print float(token), len(listStack)
+            if len(listStack) is 0:
+                return float(token)
+
+            listStack[-1].append(float(token))
+
         else:
+            print token
             if len(listStack) is 0:
                 return Symbol(token)
 
--- symbol.py
+++ symbol.py
@@ -1,3 +1,10 @@
-class symbol(object):
+class Symbol(object):
     def __init__(self, symbolName):
         self.symbolName = symbolName;
+
+    def __repr__(self):
+        return ':' + self.symbolName
+
+    def __cmp__(self, other):
+        return self.symbolName == other.symbolName
+
